dea0811c7c8c67edd59ee0bef09c873dba7088af
jojo2829 <joulinli@gmail.com>
2021-06-03 02:46:38 +0000

    initial commit

---
+++ fetcher.js
@@ -0,0 +1,29 @@
+const request = require('request');
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+const download = (url, dest, cb) => {
+  request(url, (error, response, body) => {
+    if (error) {
+      console.log('request err: ',error);
+    }
+    cb(dest, body);
+  })
+};
+
+function writeToLocal(dest, body) {
+  fs.writeFile(dest, body, err => {
+
+    if (err) {
+      console.error('write to file err: ', err)
+      return `Error`;
+    }
+    console.log(`Downloaded and saved to ${args[1]}`) //file written successfully
+  })
+}
+
+
+
+
+
+download(args[0], args[1], writeToLocal);
